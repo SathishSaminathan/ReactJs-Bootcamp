0b3d25c194fabf3024ddf4483c89e27f27ce0e3f
SathishSaminathan <sathishcse1975@gmail.com>
2021-02-24 14:06:39 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <h1 className="heading-section">Header Component</h1>;
+};
+
+export default Header;
---
+++ src/components/Sidenav.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Sidenav = () => {
+  return (
+    <div className="links">
+      <a className="links-section" href="https://www.facebook.com">
+        Facebook
+      </a>
+      <a href="https://wwww.instagram.com">Instagram </a>
+    </div>
+  );
+};
+
+export default Sidenav;
